8bda67de6269f8dae4a9ab30b2e3ddf7e5d49d16
pascualbeatriz <pascuals.beatriz@gmail.com>
2019-11-04 18:46:33 +0100

    Add docs

---
+++ docs/precache-manifest.a794b078bea4bccc5e32b262390a81c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905cc7b05009613f1209ed163bca6348",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16588c2681035854919a",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cb2a223e1514b89d2c93",
+    "url": "/static/js/2.75084fc2.chunk.js"
+  },
+  {
+    "revision": "16588c2681035854919a",
+    "url": "/static/js/main.00f66df3.chunk.js"
+  },
+  {
+    "revision": "a961f7cd1549a964071e",
+    "url": "/static/js/runtime-main.f7408956.js"
+  }
+]);
